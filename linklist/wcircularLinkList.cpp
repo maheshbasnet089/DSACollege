10b2364dc798748643730e33fd46efefda3425e5
maheshbasnet089 <basnetmanish089@gmail.com>
2023-07-10 07:58:49 +0545

    Learnt about doublyCircularLink List

--- linklist/wcircularLinkList.cpp
+++ linklist/wcircularLinkList.cpp
@@ -1,188 +1,279 @@
-Keep tapping! ??
-nggamming
-Title
 #include<stdio.h>
-#include<conio.h>
 #include<stdlib.h>
-#define MAX 100
-#include<string.h>
-struct student 
+#include<conio.h>
+struct node
 {
-
-	int roll;
-	char name[50];
-	int age;	
-	
+	int data;
+	struct node*next ;
+	struct node*prev;
 };
-struct node 
-{
-	struct student std;
-	struct node *next;
-	
-};
+struct node *header=NULL; //first node lai pointer garnea external pointer declared as null ..GLOBAL
 
-struct node *header=NULL; //empty list denotion
-struct node* getnode(struct student s)
+struct node *getnode(int n)
 {
-	struct node*ptrnew=(struct node*)malloc(sizeof(struct node)); //  getnodeprovides new node
+	struct node *ptrnew=(struct node*)malloc(sizeof(struct node));
 	if(ptrnew==NULL)
 	{
-		printf("\nMemory Allocation fail..");
+		printf("\nFAILED");
 		getch();
 		exit(0);
 	}
-	
-	/*ptrnew->std.roll=s.roll;
-	strcpy(ptrnew->std.names=s.name);
-	ptrnew->std.age=s.age;
-	ptrnew->next=NULL;
-	return ptrnew; */
-	
-	ptrnew->std=s;
-	ptrnew->next=NULL;
+	ptrnew->data=n;
+	ptrnew->next=ptrnew->prev=NULL; //front and back both null
 	return ptrnew;
 }
-
-void addStudentAtFront()
+void insertAtFront()
 {
-	struct student s;
-	struct node*ptrnew;
-	printf("\nEnter Name Of student:\n");
-	gets(s.name);
-	printf("\nEnter Roll Number:\n");
-	scanf("%d",&s.roll);
-	printf("\nENter Age of Student:\n");
-	scanf("%d",&s.age);
-	ptrnew=getnode(s);
-	
-	if(header==NULL)
+	struct node *ptrnew;
+	int n;
+	printf("\nEnter Number:");
+	scanf("%d",&n);
+	ptrnew=getnode(n);
+	if(header!=NULL)
 	{
-		header=ptrnew;
-		header->next=header; //circle 
+		ptrnew->next=header;
+		header->prev=ptrnew;
 	}
-	
+	header=ptrnew;
+	printf("\nNumber inserted at front");
+}
+void insertAtBack()
+{
+	struct node *ptrnew,*ptrthis;
+	int n;
+	printf("\nEnter Number:");
+	scanf("%d",&n);
+	ptrnew=getnode(n);
+	if(header==NULL)
+	header=ptrnew;
 	else
 	{
-		ptrnew->next=header->next;
-		header->next=ptrnew;
-		
+		for(ptrthis=header;ptrthis->next!=NULL;ptrthis=ptrthis->next);
+		ptrthis->next=ptrnew;
+		ptrnew->prev=ptrthis;
 	}
-	printf("\ninserted In Front");
+	printf("\nNode Inserted At The End.");
 }
 
+void atBack()
+{
+    int n, target;
+    struct node* ptrnew, * ptrthis;
+
+    printf("\nEnter Number After Which you want to insert: ");
+    scanf("%d", &target);
+
+    for (ptrthis = header; ptrthis != NULL; ptrthis = ptrthis->next)
+    {
+        if (ptrthis->data == target)
+        {
+            printf("\nEnter Data to Insert in the Middle: ");
+            scanf("%d", &n);
+            ptrnew = getnode(n);
+            ptrnew->next = ptrthis->next;
+            ptrnew->prev = ptrthis;
+            if (ptrthis->next != NULL)
+                (ptrthis->next)->prev = ptrnew;
+            ptrthis->next = ptrnew;
+            return;
+        }
+    }
+    printf("\nNot Found Number");
+}
+
+void insertBefore()
+{
+    int n, target;
+    struct node* ptrnew, * ptrthis;
+
+    printf("\nEnter Number Before Which you want to insert: ");
+    scanf("%d", &target);
+
+
+    if (header->data == target)
+    {
+        printf("\nEnter Data to Insert at the Beginning: ");
+        scanf("%d", &n);
+        ptrnew = getnode(n);
+        ptrnew->next = header;
+        header->prev = ptrnew;
+        header = ptrnew;
+        return;
+    }
+
+    for (ptrthis = header; ptrthis != NULL; ptrthis = ptrthis->next)
+    {
+        if (ptrthis->data == target)
+        {
+            printf("\nEnter Data to Insert in the Middle: ");
+            scanf("%d", &n);
+            ptrnew = getnode(n);
+            ptrnew->next = ptrthis;
+            ptrnew->prev = ptrthis->prev;
+            (ptrthis->prev)->next = ptrnew;
+            ptrthis->prev = ptrnew;
+            return;
+        }
+    }
+    printf("\nNot Found Number");
+}
 void display()
 {
-	struct node*ptrthis;
-	if(header==NULL)
+	struct node* ptrthis;
+	if (header == NULL)
 	{
-		printf("\nLIst Is Empty.");
+		printf("\nEMPTY");
 	}
 	else
 	{
-		printf("\nList Contains :\nRoll\tName\tAge\n");
-		ptrthis=header->next;//begin from first node
-		do
+		for (ptrthis = header; ptrthis != NULL; ptrthis = ptrthis->next)
 		{
-			
-			printf("%d\t%s\t%d\n",ptrthis->std.roll,ptrthis->std.name,ptrthis->std.age);
-			ptrthis=ptrthis->next;  
-		}while(ptrthis!=header->next); //until we reaqch the first node again
-	}
-}
-
-void removeFromFront()
-{
-	struct node*ptrthis;
-	if(header==NULL)
-	{
-		printf("\nList Empty.");
-		return;
-	}
-	
-	else if(header==header->next) //on;y one node
-	{
-		ptrthis=header;
-		header=NULL;
-	}
-	else
-	{
-		ptrthis=header->next ; //its node to bwe removed
-		header->next=ptrthis->next; //bypass ptrthis....
-	}
-	free(ptrthis);
-		printf("\nRmoved From Front.");
-}
-
-void removeFromLast()
-{
-	
-	struct node*ptrthis;
-	if(header==NULL)
-	{
-		printf("\nEmpty.");
-		return;
-	}
-
-	if(header=header->next)
-	{
-		ptrthis=header;
-		header=NULL;
-	}
-	
-	else
-	{
-		for(ptrthis=header->next;ptrthis->next!=header;ptrthis=ptrthis->next);
-		header=ptrthis;
-		ptrthis=ptrthis->next;
-		header->next=ptrthis->next;
-	}
-	free(ptrthis);
-	printf("\nRemove From Last");
-}
-
-void InsertAtLast()
-{
-		struct student s;
-		struct node*ptrnew;
-	printf("\nEnter Name Of student");
-	gets(s.name);
-	printf("\nEnter Roll Number.");
-	scanf("%d",&s.roll);
-	printf("\nENter Age of Student.");
-	scanf("%d",&s.age);
-	ptrnew=getnode(s);
-	
-	if(header==NULL)
-	{
-		header=ptrnew;
-		header->next=header; //circle 
-	}
-	
-	else
-	{
-		ptrnew->next=header->next;
-		header->next=ptrnew;
-		header=header->next;
-	}
-	
-	printf("\nInsert St end of list..");
-}
-int main()
-{
-	char ch;
-	while(1)
-	{
-	printf("Select Your Option:\n1.Insert At Front \n2.Insert At Last.\n3.Insert After.\n4.InsertBefore\n5.Remove From Front\n");
-	printf("6.Remove From Last\n7. Remove Any.\n8.Display All.\n9.Exit\n");	
-	fflush(stdin);
-	ch=getche();	
-		switch(ch)
-		{
-			case '1': addStudentAtFront(); break;
-			case '2' :InsertAtLast();break;
-			case'6' : removeFromFront();break;
- 			case '8': display();break;
-			case '9':  return 0;
+			printf("%d\t", ptrthis->data);
 		}
 	}
 }
+
+void removefromfront()
+
+{
+	
+	
+	 int n, target;
+    struct node* ptrthis;
+    if(header==NULL)
+    {
+    	printf("\nList Empty.");
+	}
+
+	else
+	{
+		
+		ptrthis=header;
+		header=header->next;
+		if(header!=NULL)
+		{
+			header->prev=NULL; //condition in one then more ..
+		}
+		free(ptrthis);
+		printf("\nNode Removed From Front.");
+	}
+}
+  	void removeFromLast()
+  	{
+  	
+	  
+	  struct node *ptrthis; //to search last node
+	  if(header==NULL)
+	  	{
+	  		printf("\nList Empty.");
+	  		
+		 }
+	else
+	{
+		for(ptrthis=header;ptrthis->next!=NULL;ptrthis=ptrthis->next);
+		if(ptrthis==header)
+		{
+		header=NULL;
+			
+		}	
+		else
+		
+			(ptrthis->prev)->next=NULL;
+			free(ptrthis);
+			printf("\nNode Removed from Last.");
+				
+	}
+		  
+	}
+   
+	void removeAny()
+	{
+		
+		struct node *ptrthis,*ptrprev;
+		int target;
+		printf("\nEnter A Number to be remove:\n");
+		scanf("%d",&target);
+		for(ptrthis=header;ptrthis!=NULL;ptrthis=ptrthis->next);
+		{
+			if(ptrthis->data==target)
+			{
+				if(ptrthis==header) //first node is gonna be removed 
+				{
+					
+					header=header->next;
+					if(header!=NULL)
+						header->prev=NULL;
+				}
+					else if(ptrthis->next==NULL) // last node ...
+					
+					(ptrthis->prev)->next=NULL;	
+					
+				}
+				else //intermidate nodes
+				{
+					
+					(ptrthis->prev)->next=ptrthis->next;
+					(ptrthis->next)->prev=ptrthis->prev;
+					
+				}
+				free(ptrthis);
+				printf("\nNode Removed.");
+				return;		
+			} //if node found then delete it and return 
+			printf("\nNot Found.");
+		}
+		
+		
+
+	
+
+int main()
+{
+	char ch;
+	while (1)
+	{
+		system("cls");
+		printf("\nSELECT OPTION\n1.InsertAtfront\n2.InsertAtBack\n3.AtBack\n4.insertBefore\n5.Display\n6.removefromfront\n");
+		printf("7.removeFromLast\n8.RemoveANy\n9.Exit\n\n");
+		ch = getche();
+		switch (ch)
+		{
+		case '1':
+			insertAtFront();
+			break;
+		case '2':
+			insertAtBack();
+			break;
+		case '3':
+			atBack();
+			break;
+		case '4':
+			insertBefore();
+			break;
+			
+			case '5':
+				printf("\n");
+			display();
+			break;
+			
+				case '6':
+				removefromfront();
+			break;
+			
+			case '7':
+				removeFromLast();
+				break;
+				
+			case'8':
+				removeAny();
+				break;
+				
+		default:
+			printf("\nInvalid Option");
+			break;
+		}
+		getch();
+	}
+}
+
+
